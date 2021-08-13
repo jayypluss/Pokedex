8759a5375bab22a5cbfb54e7c031d0e850e31c94
Jonathan Ferreira Kuntz <jonathanfk1@gmail.com>
2021-08-12 22:54:03 -0300

    Adding BabylonJS to project.

--- src/pages/PokeDetails/index.tsx
+++ src/pages/PokeDetails/index.tsx
@@ -1,5 +1,5 @@
 // libraries
-import React from 'react';
+import React, {FunctionComponent, useEffect, useState} from 'react';
 import {View, ScrollView, Text, Image} from 'react-native';
 import {Divider} from 'react-native-elements';
 
@@ -14,6 +14,57 @@ import TypeChip from '../../components/BaseComponents/TypeChip';
 import {PokemonAbilityItem} from '../../models/Ability';
 import {PokemonStatItem} from '../../models/Stat';
 
+import {EngineView, useEngine} from '@babylonjs/react-native';
+import {
+  ArcRotateCamera,
+  Camera,
+  Color3,
+  Engine, Mesh,
+  PBRMetallicRoughnessMaterial,
+  Scene,
+  SceneLoader,
+  TransformNode,
+  Vector3
+} from '@babylonjs/core';
+
+
+interface MyComponentProps {
+
+}
+
+const MyComponent: FunctionComponent<MyComponentProps> = (props: MyComponentProps) => {
+
+  const engine = useEngine();
+  const [camera, setCamera] = useState<Camera>();
+
+  useEffect(() => {
+    if (engine) {
+      const scene = new Scene(engine);
+      scene.createDefaultCamera(true);
+      (scene.activeCamera as ArcRotateCamera).beta -= Math.PI / 8;
+      setCamera(scene.activeCamera!);
+      scene.createDefaultLight(true);
+
+      const box = Mesh.CreateBox("box", 0.3, scene);
+      const mat = new PBRMetallicRoughnessMaterial("mat", scene);
+      mat.metallic = 1;
+      mat.roughness = 0.5;
+      mat.baseColor = Color3.Red();
+      box.material = mat;
+
+      scene.beforeRender = function () {
+        box.rotate(Vector3.Up(), 0.005 * scene.getAnimationRatio());
+      };
+    }
+  }, [engine]);
+
+  return (
+      <View style={{width: '80%', height: 400}}>
+          <EngineView style={{flex: 1}} camera={camera} />
+      </View>
+  );
+}
+
 function PokeDetails({route}: any) {
   const {pokemon} = route.params;
 
@@ -26,6 +77,8 @@ function PokeDetails({route}: any) {
         contentContainerStyle={styles.contentContainerStyle}>
         <View style={styles.pokemonContainer}>
           <Image source={{uri: pokemon.spriteUrl}} style={styles.image} />
+
+          <MyComponent />
 
           <View style={[styles.infoContainer, styles.numberContainer]}>
             <Text style={styles.id}># {pokemon.id?.padStart(3, '0')}</Text>
